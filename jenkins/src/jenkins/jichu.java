2bab3504716f7c8c9a507e1337d52583a81f15e9
zhangzor <2450169510@qq.com>
2019-03-12 14:53:48 +0800

    2019.03.12下午14点53分

---
+++ jenkins/src/jenkins/jichu.java
@@ -0,0 +1,16 @@
+package jenkins;
+
+public class jichu {
+	public static void main(String[] args) {
+		int a,b,c;
+		a=2;b=5;
+		c=a+b;
+		System.out.println("加法："+c);
+		c=a-b;
+		System.out.println("减法: "+c);
+		c=a*b;
+		System.out.println("乘法: "+c);
+		c=a/b;
+		System.out.println("除法："+c);
+	}
+}
